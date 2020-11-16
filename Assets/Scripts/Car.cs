0291b852e8df81a031e18719179aad359e86ec89
Jet Yong <jettlag2000@gmail.com>
2020-11-16 15:22:30 +1300

    Nov 16th Commit

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,35 +5,56 @@ using UnityEngine.UI;
 
 public class Car : MonoBehaviour
 {
-    public GameObject fuelcan;
-    public float speed;
-    public float startSpeed;
+    public WheelJoint2D wheel1;
+    public float CurrentSpeed;
+    public float SpeedSetting;
     public float currentFuel;
-    public float maxFuel;
+    public float consumptionRate;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * CurrentSpeed * Time.deltaTime);
 
         if (currentFuel <= 0)
         {
-            speed = 0;
+            CurrentSpeed = 0;
         }
 
-        if (speed >= 0.1)
+        if (wheel1.jointSpeed != 0)
         {
-            currentFuel -= 1 * Time.deltaTime;
+            currentFuel -= Time.deltaTime;
         }
 
-        if (currentFuel >=0.1)
+        if (currentFuel <= 0)
         {
-            speed = startSpeed;
+            CurrentSpeed = 0;
+        }
+        else
+        {
+            CurrentSpeed = SpeedSetting;
         }
 
-        if (currentFuel >150)
+        if (currentFuel >1500)
         {
-            currentFuel = 150;
+            currentFuel = 1500; //turn off intake
         }
+
+        if(currentFuel < 0)
+        {
+            currentFuel = 0;
+        }
+    }
+
+    public void LowGear()
+    {
+        SpeedSetting = 2;
+        consumptionRate = 1;
+    }
+
+    public void HighGear()
+    {
+        SpeedSetting = 5;
+        consumptionRate = 3;
     }
 }
---
+++ Assets/Scripts/TankParts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankParts : MonoBehaviour
+{
+    public float health;
+    public bool broken = false;
+    SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(health <=0)
+        {
+            broken = true;
+        }
+
+        if(broken == true)
+        {
+            sprite.color = new Color(1, 0, 0, 1);
+        }
+        
+        if(health >=100)
+        {
+            sprite.color = new Color(1, 1, 1, 1);
+        }
+
+        if(health >= 100)
+        {
+            health = 100;
+        }
+    }
+}
